c772162d34767421221258b46fd4868ada08d5d0
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-30 13:34:00 -0700

    seperated all monsters by type 1

--- main.py
+++ main.py
@@ -49,6 +49,71 @@ normal_type = df.loc[df['Type 1'] == 'Normal']
 # Saving normal type to a csv
 normal_type.to_csv('./CSV/normal.type.csv')
 
+# Poison type monsters
+poison_type = df.loc[df['Type 1'] == 'Poison']
+# Saving poison type to csv
+poison_type.to_csv('./CSV/poison.type.csv')
+
+# Electric type monsters
+electric_type = df.loc[df['Type 1'] == 'Poison']
+# Saving electric type tp csv
+electric_type.to_csv('./CSV/electric.type.csv')
+
+# Ground type monsters
+ground_type = df.loc[df['Type 1'] == 'Ground']
+# Saving ground type monsters to a csv file
+ground_type.to_csv('./CSV/ground.type.csv')
+
+# Fairy type monsters
+fairy_type = df.loc[df['Type 1'] == 'Fairy']
+# Saving fairy type to a csv file
+fairy_type.to_csv('./CSV/fairy.type.csv')
+
+# Fighting type monsters
+fighting_type = df.loc[df['Type 1'] == 'Fighting']
+# Saving fighting type to a csv file
+fighting_type.to_csv('./CSV/fighting.type.csv')
+
+# Psychic type monsters
+psychic_type = df.loc[df['Type 1'] == 'Psychic']
+# Saving psychic type to a csv file
+psychic_type.to_csv('./CSV/psychic.type.csv')
+
+# Rock type monsters
+rock_type = df.loc[df['Type 1'] == 'Rock']
+# Saving rock type monsters to a csv file
+rock_type.to_csv('./CSV/rock.type.csv')
+
+# Ghost type monsters
+ghost_type = df.loc[df['Type 1'] == 'Ghost']
+# Saving ghost type to a csv file
+ghost_type.to_csv('./CSV/ghost.type.csv')
+
+# Ice type monsters
+ice_type = df.loc[df['Type 1'] == 'Ice']
+# Saving ice type to a csv file
+ice_type.to_csv('./CSV/ice.type.csv')
+
+# Dragon type monsters
+dragon_type = df.loc[df['Type 1'] == 'Dragon']
+# Saving dragon type monsters to a csv file
+dragon_type.to_csv('./CSV/dragon.type.csv')
+
+# Dark type monsters
+dark_type = df.loc[df['Type 1'] == 'Dark']
+# Sacving dark type to a csv file
+dark_type.to_csv('./CSV/dark.type.csv')
+
+# Steel type monsters
+steel_type = df.loc[df['Type 1'] == 'Steel']
+# Saving steel type to a csv file
+steel_type.to_csv('./CSV/steel.type.csv')
+
+# Flying type monsters
+flying_type = df.loc[df['Type 1'] == 'Flying']
+# Saving flying type to a csv file
+flying_type.to_csv('./CSV/flying.type.csv')
+
 
 
 
