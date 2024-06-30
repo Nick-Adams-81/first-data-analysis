86687e9364aae47178feb02eeabb1963087b947d
Nicholas Adams <nicholasadams@Owners-MBP.lan>
2024-06-30 13:16:32 -0700

    refactored csv files into its own directory

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ df['Overall Value'] = overall_value
 df['Overall Value'] = df['Overall Value'].round(0)
 
 # Writing the new overall value data to a csv file.
-df.to_csv('overall_value.csv')
+df.to_csv('./CSV/overall_value.csv')
 
 # Seperating monsters by type 1, as not all creatures have a type 2
 # Grass, Fire, Water, Bug, Normal, Poison, Electric, Ground, Fairy, 
@@ -27,7 +27,29 @@ df.to_csv('overall_value.csv')
 # Grass type monsters
 grass_type = df.loc[df['Type 1'] == 'Grass']
 # Saving grass type monsters to a csv file
-grass_type.to_csv('grass.type.csv')
+grass_type.to_csv('./CSV/grass.type.csv')
+
+# Fire type monsters
+fire_type = df.loc[df['Type 1'] == 'Fire']
+# Saving fire type to a csv file
+fire_type.to_csv('./CSV/fire.type.csv')
+
+# Water type monsters
+water_type = df.loc[df['Type 1'] == 'Water']
+# saving water type monsters to a csv file
+water_type.to_csv('./CSV/water.type.csv')
+
+# Bug type mpnsters
+bug_type = df.loc[df['Type 1'] == 'Bug']
+# Sacing bug type to a csv file
+bug_type.to_csv('./CSV/bug.type.csv')
+
+# Normal type monsters
+normal_type = df.loc[df['Type 1'] == 'Normal']
+# Saving normal type to a csv
+normal_type.to_csv('./CSV/normal.type.csv')
+
+
 
 
 
